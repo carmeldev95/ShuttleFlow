cf36b4b7619a1afef08560ca80fb04e5f28f9260
carmeldev95 <carmeldev95@gmail.com>
2026-03-05 19:44:50 +0200

    git add shuttle-flow-client/vercel.json git commit -m "Add Vercel routes for SPA" git push

--- shuttle-flow-server/src/app.js
+++ shuttle-flow-server/src/app.js
@@ -21,7 +21,9 @@ export function createApp() {
     env.clientOrigin,               // e.g. http://localhost:5173
     "http://127.0.0.1:5173",
     "http://localhost:5174",
+    "http://localhost:5173",
     "http://127.0.0.1:5174",
+    "http://localhost:4173"
   ]);
 
   app.use(
